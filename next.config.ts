27ad81cce2c70b5651b8c0ed4a4920eea6edf870
Larry04Dani <danieleata04@gmail.com>
2026-08-24 19:16:41 +0200

    First Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import nextMDX from '@next/mdx'
+import type { NextConfig } from 'next'
+
+const withMDX = nextMDX()
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+}
 
-export default nextConfig;
+export default withMDX(nextConfig)
